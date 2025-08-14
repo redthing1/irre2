4a43d70f7f833744c13e39a4d8e35e0ce5dc6036
redthing1 <redthing1@alt.icu>
2025-08-14 13:12:10 -0700

    instruction formatting

--- src/irre_core/util.hpp
+++ src/irre_core/util.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "instruction.hpp"
+#include "types.hpp"
+#include <cstdio>
+#include <string>
 #include <variant>
 
 namespace irre {
@@ -33,4 +37,38 @@ public:
     }
   }
 };
+
+// instruction formatting
+inline std::string format_instruction(const instruction& inst) {
+  return std::visit([](const auto& i) -> std::string {
+    const auto mnemonic = get_mnemonic(i.op);
+    
+    if constexpr (std::is_same_v<std::decay_t<decltype(i)>, inst_op>) {
+      return mnemonic;
+    } else if constexpr (std::is_same_v<std::decay_t<decltype(i)>, inst_op_reg>) {
+      return std::string(mnemonic) + " " + reg_name(i.a);
+    } else if constexpr (std::is_same_v<std::decay_t<decltype(i)>, inst_op_imm24>) {
+      char buf[32];
+      std::snprintf(buf, sizeof(buf), "%s 0x%06x", mnemonic, i.addr);
+      return buf;
+    } else if constexpr (std::is_same_v<std::decay_t<decltype(i)>, inst_op_reg_imm16>) {
+      char buf[32];
+      std::snprintf(buf, sizeof(buf), "%s %s 0x%04x", mnemonic, reg_name(i.a), i.imm);
+      return buf;
+    } else if constexpr (std::is_same_v<std::decay_t<decltype(i)>, inst_op_reg_reg>) {
+      return std::string(mnemonic) + " " + reg_name(i.a) + " " + reg_name(i.b);
+    } else if constexpr (std::is_same_v<std::decay_t<decltype(i)>, inst_op_reg_reg_imm8>) {
+      char buf[32];
+      std::snprintf(buf, sizeof(buf), "%s %s %s 0x%02x", mnemonic, reg_name(i.a), reg_name(i.b), i.offset);
+      return buf;
+    } else if constexpr (std::is_same_v<std::decay_t<decltype(i)>, inst_op_reg_imm8x2>) {
+      char buf[32];
+      std::snprintf(buf, sizeof(buf), "%s %s 0x%02x 0x%02x", mnemonic, reg_name(i.a), i.v0, i.v1);
+      return buf;
+    } else if constexpr (std::is_same_v<std::decay_t<decltype(i)>, inst_op_reg_reg_reg>) {
+      return std::string(mnemonic) + " " + reg_name(i.a) + " " + reg_name(i.b) + " " + reg_name(i.c);
+    }
+  }, inst);
+}
+
 } // namespace irre
